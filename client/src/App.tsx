4fef1cc01232aa8039106f40d37acaa1f9324492
Shezan Fayaz <shezanf7@gmail.com>
2026-04-04 18:34:10 +0530

    Quiz View Page Create

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import AuthPage from "./pages/AuthPage"
 import LandingPage from "./pages/LandingPage"
 import { Routes, Route } from "react-router-dom"
+import QuizViewPage from "./pages/QuizViewPage"
+
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<AuthPage />} />
         <Route path="/login" element={<AuthPage />} />
+
+        <Route path="/quiz/:id" element={<QuizViewPage />} />
       </Routes>
     </>
   )
